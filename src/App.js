48b5e96f9965eed44046249961d607092ee6c997
Semih <semihozden01@gmail.com>
2020-10-14 12:26:00 +0300

    highlight style added and new functional component created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
+function Tab({children,className,...restProps}){
+  const [highlightStyle, sethighlightStyle] = useState({left:0, opacity:0});
+
+  //update highlight to move the highlight
+  function moveHighlight(e){
+    sethighlightStyle({
+      left:e.nativeEvent.layerX-150,
+    })
+  }
+
+  function hideHighlight(e){
+    sethighlightStyle({
+      left:e.nativeEvent.layerX-150,
+      opacity:0
+    })
+  }
+  return (
+    // take className as props and concanate them as a simple class
+    <div {...restProps} className={`tab ${className}`} onMouseOut={hideHighlight} onMouseMove={moveHighlight}>
+      <div className="highlight" style={highlightStyle}/>
+      <a>{restProps.name}</a>
+      {children}
+    </div>
+  );
+}
+
 function App() {
+
+
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab">
-            <a>Home</a>
-          </div>
-          <div className="tab">
-            <a>About</a>
-          </div>
-          <div className="tab">
-            <a>Features</a>
-          </div>
+          <Tab name="Home" className="hey"/>
+          <Tab name="About"/>
+          <Tab name="Features"/>
         </div>
 
         <div className="viewport">Pages Go Here</div>
